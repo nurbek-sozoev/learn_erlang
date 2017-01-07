b04836b9e7c70221f12f5459c09c06ab27aa5453
Sozoev Nurbek <sozoev@gmail.com>
2017-01-07 23:29:24 +0600

    Добавил проект fizzbuzz

---
+++ fizzbuzz/src/fizzbuzz_app.erl
@@ -0,0 +1,27 @@
+-module(fizzbuzz_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    fizzbuzz_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+-ifdef(TEST).
+
+simple_test() ->
+    ok = application:start(fizzbuzz),
+    ?assertNot(undefined == whereis(fizzbuzz_sup)).
+
+-endif.
